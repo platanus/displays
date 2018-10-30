6acc71d5ce9fed69bcc3c090a4fb173c7453f1e3
Juan Ignacio Donoso <jidonoso@gmail.com>
2018-10-30 18:58:40 -0300

    chore(ping): set ping inverval with env variables

--- config/clock.rb
+++ config/clock.rb
@@ -1,12 +1,14 @@
 require 'clockwork'
 require './config/environment'
 
+SSE_PING_INTERVAL = ENV.fetch('SSE_PING_INTERVAL').to_i.seconds
+
 module Clockwork
   handler do |job|
     puts "Running #{job}"
   end
 
-  every(2.seconds, 'display.ping', thread: true) do
+  every(SSE_PING_INTERVAL, 'display.ping', thread: true) do
     redis = Redis.new
     redis.publish('display:ping', Time.now)
   end
