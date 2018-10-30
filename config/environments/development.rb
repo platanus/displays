c9aea3bb93d1619bc9eaf9c88209bc56c7e09f7b
Juan Ignacio Donoso <jidonoso@gmail.com>
2018-10-30 13:59:52 -0300

    chore(sse): change development cacha classes config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.cache_classes = true
+  config.cache_classes = false
   config.eager_load = true
   config.consider_all_requests_local = true
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
