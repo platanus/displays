1ccd3f5467be3329c14f4125261fe3f6b8d6829a
Juan Ignacio Donoso <jidonoso@gmail.com>
2018-10-30 17:22:32 -0300

    feat(ping): use clockwork to schedule the ping event

---
+++ config/clock.rb
@@ -0,0 +1,13 @@
+require 'clockwork'
+require './config/environment'
+
+module Clockwork
+  handler do |job|
+    puts "Running #{job}"
+  end
+
+  every(2.seconds, 'display.ping', thread: true) do
+    redis = Redis.new()
+    redis.publish('display:ping', Time.now)
+  end
+end
