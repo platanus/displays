0af885815383e285dde32f421c2335bd4a2a7f53
Juan Ignacio Donoso <jidonoso@gmail.com>
2018-10-31 16:35:23 -0300

    chore(webpack): install vue support for webpacker

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -16,6 +16,9 @@ Rails.application.config.content_security_policy do |policy|
 #   # policy.report_uri "/csp-violation-report-endpoint"
   if Rails.env.development?
     policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035"
+    policy.script_src :self, :https, :unsafe_eval
+  else
+    policy.script_src :self, :https
   end
 end
 
