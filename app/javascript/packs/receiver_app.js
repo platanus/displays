cc9f8d7ebe2a30eff34ccb3c4ad5a48fba9f6c77
Juan Ignacio Donoso <jidonoso@gmail.com>
2018-11-06 11:01:01 -0300

    fix(display): naming

--- app/javascript/packs/receiver_app.js
+++ app/javascript/packs/receiver_app.js
@@ -1,11 +1,11 @@
 /* global document */
 
 import Vue from 'vue/dist/vue.esm';
-import RemoteIframe from '../components/remote_iframe.vue';
+import Display from './components/display.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({ // eslint-disable-line no-new
     el: '#receiver_app',
-    components: { RemoteIframe },
+    components: { Display },
   });
 });
