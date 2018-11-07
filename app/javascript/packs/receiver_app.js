e4b8a9e0531febe437df47f300f05b3868454efd
Juan Ignacio Donoso <jidonoso@gmail.com>
2018-11-06 22:58:52 -0300

    fix(display): use render function with runtime vue dist

--- app/javascript/packs/receiver_app.js
+++ app/javascript/packs/receiver_app.js
@@ -1,11 +1,14 @@
 /* global document */
 
-import Vue from 'vue/dist/vue.esm';
+import Vue from 'vue/dist/vue.runtime.esm';
 import Display from './components/display.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({ // eslint-disable-line no-new
     el: '#receiver_app',
     components: { Display },
+    render(createElement) {
+      return createElement(Display);
+    },
   });
 });
