85f498ce693eefc1b137d285042d7bfcfa8b6068
Juan Ignacio Donoso <jidonoso@gmail.com>
2018-11-07 08:47:09 -0300

    refactor(receiver): remove sprockets and only use modules for stlyes

--- app/javascript/packs/receiver_app.js
+++ app/javascript/packs/receiver_app.js
@@ -1,14 +1,13 @@
 /* global document */
 
 import Vue from 'vue/dist/vue.runtime.esm';
-import DisplayContainer from '../components/display_container.vue';
+import ReceiverApp from '../components/receiver_app.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({ // eslint-disable-line no-new
     el: '#receiver_app',
-    components: { DisplayContainer },
     render(createElement) {
-      return createElement(DisplayContainer);
+      return createElement(ReceiverApp);
     },
   });
 });
